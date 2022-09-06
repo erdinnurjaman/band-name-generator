e66b5d83201c38c49b441b196a5b883e69587e34
Erdin Nurjaman <111257907+erdinnurjaman@users.noreply.github.com>
2022-09-06 08:23:29 +0700

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,13 @@
+# Project day - 1 : Band Name Generator
+
+# Print judul / nama project
+print('Welcome to the Band Name Generator!!!')
+
+# Buat fungsi inputan untuk memasukan data tempat atau kota dimana kamu tinggal dan masukan ke variable "city"
+city = input('Which city did you grow up in?\n')
+
+# Buat fungsi inputan untuk memasukan data nama hewan peliharaanmu dan masukan ke variable "pet"
+pet = input('What is the name of your pet?\n')
+
+# Gunakan fungsi print untuk memunculkan hasil dari generator ini untuk menghasilkan kombinasi nama dengan menambahkan beberapa string dan masukan variable "city" dan "pet"
+print('Your band name is ' + city + ' ' + pet)
